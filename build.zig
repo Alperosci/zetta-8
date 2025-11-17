0257ecc2faf336337c01d8fdc320e55e3c84d5af
Alperosci <alperosci@gmail.com>
2025-11-17 10:40:32 +0300

    optimize

--- build.zig
+++ build.zig
@@ -1,22 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    //const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{.preferred_optimize_mode = .ReleaseFast});
     const exe = b.addExecutable(.{
         .name = "zetta8",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            //.optimize = optimize,
+            .target = b.graph.host,
+            .optimize = optimize,
         }),
     });
 
-    exe.root_module.addIncludePath(.{ .cwd_relative = "/usr/local/include/" });
-    exe.root_module.addLibraryPath(.{ .cwd_relative = "/usr/local/bin/" });
-
     exe.linkSystemLibrary("raylib");
     exe.linkSystemLibrary("m");
+    exe.linkLibC();
 
     b.installArtifact(exe);
 
